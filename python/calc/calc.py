30b0df836758419592e0e67618b5e02e526c1f48
vottie <tsuboi23@gmail.com>
2015-08-07 01:53:55 +0900

    class method call from another file.

--- python/calc/calc.py
+++ python/calc/calc.py
@@ -2,12 +2,6 @@ class Calc:
     # need self.
     # occur below
     # typeError: add() takes exactly 2 arguments (3 given)
-    def add(x, y):
+    def add(self, x, y):
         return x + y
 
-c = Calc()
-x = 100
-y = 200
-result = 0
-result = c.add(x,y)
-print '{0} + {1} = {2}'.format(x, y, result)
---
+++ python/calc/test.py
@@ -0,0 +1,8 @@
+from calc import Calc
+
+c = Calc()
+x = 100
+y = 200
+result = 0
+result = c.add(x,y)
+print '{0} + {1} = {2}'.format(x, y, result)
