a500629592d693b1906c8e01e873c9005b0f538e
vottie <tsuboi23@gmail.com>
2015-08-23 01:24:06 +0900

    Add csv reader.

---
+++ python/reflection/csv_reader.py
@@ -0,0 +1,16 @@
+import csv
+
+class CSVReader:
+    def __init__(self, f):
+        self.f = f
+
+    def parse(self):
+        with open(self.f, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+
+            for row in reader:
+                print(row)
+
+
+c = CSVReader('sample.csv')
+c.parse()
