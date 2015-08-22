e058a0f41b07a94d41a8672657ecb241fe2ce0fe
vottie <tsuboi23@gmail.com>
2015-08-23 01:19:51 +0900

    Add text reader.

---
+++ python/reflection/txt_reader.py
@@ -0,0 +1,15 @@
+class TxtReader:
+    def __init__(self, f):
+        self.f = f
+
+    def parse(self):
+        file = open(self.f)
+        line = file.readline()
+        
+        while line:
+            print(line)
+            line = file.readline()
+        file.close
+
+t = TxtReader('sample.txt')
+t.parse()
