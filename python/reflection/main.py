50a89f59a3b8c1ebd17357afd9afe0149a6e18ef
vottie <tsuboi23@gmail.com>
2015-08-29 06:51:24 +0900

    Delete comment out.

--- python/reflection/main.py
+++ python/reflection/main.py
@@ -1,12 +1,8 @@
 import sys
 
-#klass = globals()['TxtReader']
-#obj = klass('sample.csv')
-#obj.parse()
-
-mod = __import__('txt_reader',fromlist=['TxtReader'])
-klass = getattr(mod, 'TxtReader')
-obj = klass('sample.txt')
+# mod = __import__('txt_reader',fromlist=['TxtReader'])
+# klass = getattr(mod, 'TxtReader')
+# obj = klass('sample.txt')
 
 mod = __import__('csv_reader',fromlist=['CSVReader'])
 klass = getattr(mod, 'CSVReader')
