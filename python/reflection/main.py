14a0cfa53e2c1db8e5fe75ecaaf0114e43a1e7dd
vottie <tsuboi23@gmail.com>
2015-08-29 06:49:22 +0900

    Modified reflection. use __import__ and getattr().

--- python/reflection/main.py
+++ python/reflection/main.py
@@ -1,8 +1,14 @@
 import sys
-from txt_reader import TxtReader
-from csv_reader import CSVReader
 
-klass = globals()['TxtReader']
+#klass = globals()['TxtReader']
+#obj = klass('sample.csv')
+#obj.parse()
+
+mod = __import__('txt_reader',fromlist=['TxtReader'])
+klass = getattr(mod, 'TxtReader')
+obj = klass('sample.txt')
+
+mod = __import__('csv_reader',fromlist=['CSVReader'])
+klass = getattr(mod, 'CSVReader')
 obj = klass('sample.csv')
 obj.parse()
-
